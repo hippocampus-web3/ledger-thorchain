f5f48ffce91d1107169acd87a86bad8651c338c2
veado <61792675+veado@users.noreply.github.com>
2021-09-08 16:39:14 +0200

    Update `extractSignatureFromTLV`  helper (#5)
    - [x] `Buffer` type
    - [x] Add test 
    - [x] Bump `0.1.0-alpha.2`

--- src/helper.ts
+++ src/helper.ts
@@ -13,6 +13,7 @@ import Ripemd160 from "ripemd160";
 import { bech32 } from "bech32";
 import { PubKeyResponse, SignResponse, VersionResponse } from "./types";
 
+
 export function serializeHRP(hrp: string): Buffer {
   if (!hrp || hrp.length < 3 || hrp.length > 83) {
     throw new LedgerError(LedgerErrorType.HPRInvalid);
@@ -163,58 +164,59 @@ export async function getPublicKey(transport: Transport, data: Buffer): Promise<
  *   2 32   -- length of s is 32 bytes
  *   <32 bytes of s>
  */
-export function extractSignatureFromTLV(signatureArray: number[]): number[] {
+export function extractSignatureFromTLV(signatureArray: Buffer): Buffer {
   // Check Type Length Value encoding
   if (signatureArray.length < 64) {
     throw Error("Invalid Signature: Too short");
   }
-  if (signatureArray[0] != 0x30) {
+  if (signatureArray[0] !== 0x30) {
     throw Error("Invalid Ledger Signature TLV encoding: expected first byte 0x30");
   }
-  if (signatureArray[1] + 2 != signatureArray.length) {
+  if (signatureArray[1] + 2 !== signatureArray.length) {
     throw Error("Invalid Signature: signature length does not match TLV");
   }
-  if (signatureArray[2] != 0x02) {
+  if (signatureArray[2] !== 0x02) {
     throw Error("Invalid Ledger Signature TLV encoding: expected length type 0x02");
   }
 
   // r signature
   const rLength = signatureArray[3];
-  var rSignature = signatureArray.slice(4, rLength + 4);
+  let rSignature = signatureArray.slice(4, rLength + 4);
   // Drop leading zero on some 'r' signatures that are 33 bytes.
-  if (rSignature.length == 33 && rSignature[0] == 0) {
+  if (rSignature.length === 33 && rSignature[0] === 0) {
     rSignature = rSignature.slice(1, 33);
-  } else if (rSignature.length == 33) {
+  } else if (rSignature.length === 33) {
     throw Error('Invalid signature: "r" too long');
   }
   // add leading zero's to pad to 32 bytes
   while (rSignature.length < 32) {
-    rSignature.unshift(0);
+    Buffer.concat([Buffer.from([0]), rSignature]);
   }
 
   // s signature
-  if (signatureArray[rLength + 4] != 0x02) {
+  if (signatureArray[rLength + 4] !== 0x02) {
     throw Error("Invalid Ledger Signature TLV encoding: expected length type 0x02");
   }
   const sLength = signatureArray[rLength + 5];
   if (4 + rLength + 2 + sLength != signatureArray.length) {
     throw Error("Invalid Ledger Signature: TLV byte lengths do not match message length");
   }
-  var sSignature = signatureArray.slice(rLength + 6, signatureArray.length);
+  let sSignature = signatureArray.slice(rLength + 6, signatureArray.length);
   // Drop leading zero on 's' signatures that are 33 bytes. This shouldn't occur since ledger signs using "Small s" math. But just to be sure...
-  if (sSignature.length == 33 && sSignature[0] == 0) {
+  if (sSignature.length === 33 && sSignature[0] === 0) {
     sSignature = sSignature.slice(1, 33);
-  } else if (sSignature.length == 33) {
+  } else if (sSignature.length === 33) {
     throw Error('Invalid signature: "s" too long');
   }
+
   // add leading zero's to pad to 32 bytes
   while (sSignature.length < 32) {
-    sSignature.unshift(0);
+    Buffer.concat([Buffer.from([0]), sSignature]);
   }
 
-  if (rSignature.length != 32 || sSignature.length != 32) {
+  if (rSignature.length !== 32 || sSignature.length !== 32) {
     throw Error("Invalid signatures: must be 32 bytes each");
   }
 
-  return rSignature.concat(sSignature);
+  return Buffer.concat([rSignature, sSignature])
 }
--- tests/unit/basic.spec.ts
+++ tests/unit/basic.spec.ts
@@ -1,4 +1,4 @@
-import { getBech32FromPK } from "../../src/index";
+import { extractSignatureFromTLV, getBech32FromPK } from "../../src/index";
 
 describe("Helpers unit", () => {
   it("check address conversion", async () => {
@@ -7,4 +7,23 @@ describe("Helpers unit", () => {
     const addr = getBech32FromPK("thor", pk);
     expect(addr).toEqual("thor1mwyrp6lj85swy5e5g4hjlaacm33g6rw3p4qmq4");
   });
+
+  it("extractSignatureFromTLV", async () => {
+    const from = [
+      48, 69, 2, 33,
+      0, 161, 207, 217, 127, 151, 190, 23, 98, 217, 186, 108, 82, 102, 19, 222, 20, 171, 6, 240, 134, 195, 251, 98, 190, 246, 228, 243, 215, 95, 166, 121, 165,
+      2, 32,
+      69, 188, 53,  213, 24, 9, 191, 90, 244, 21, 213, 146, 240, 109, 156, 221, 247, 63, 131, 52, 150, 253, 199, 153, 132, 76, 91, 239, 28, 254, 68, 80
+    ];
+
+    const to = [
+      161, 207, 217, 127, 151, 190, 23, 98, 217, 186, 108, 82, 102, 19, 222, 20, 171, 6, 240, 134, 195, 251, 98, 190, 246, 228, 243, 215, 95, 166, 121, 165,
+      69, 188, 53, 213, 24, 9, 191, 90, 244, 21, 213, 146, 240, 109, 156, 221, 247, 63, 131, 52, 150, 253, 199, 153, 132, 76, 91, 239, 28, 254, 68, 80
+    ];
+
+    const result = extractSignatureFromTLV(Buffer.from(from));
+    expect(result.equals(Buffer.from(to))).toBeTruthy();
+
+  });
+
 });
